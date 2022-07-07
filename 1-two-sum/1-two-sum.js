55cf6772ba6b96d35449534a4d4c74b3e6e53e5e
Gerardo Garcia <38366630+garcia116@users.noreply.github.com>
2022-07-07 16:10:22 -0700

    Time: 332 ms (5.86%), Space: 51.4 MB (5.20%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var map = {}
+    
+    for(let i=0; i < nums.length; i++) {
+        if(map[target-nums[i]] >= 0){
+            return [map[target-nums[i]], i]
+        } else {
+            map[nums[i]] = i;
+            console.log([map[nums[i]], i])
+        }
+    }
+
+};
