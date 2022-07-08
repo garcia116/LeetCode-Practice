59041c2e7555edae86379f37925dd95dfd708a6c
Gerardo Garcia <38366630+garcia116@users.noreply.github.com>
2022-07-07 22:29:40 -0700

    Time: 107 ms (29.83%), Space: 44.3 MB (54.42%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -4,8 +4,12 @@
  */
 var mostWordsFound = function(sentences) {
     var max = 0
-    sentences.forEach(sentence => {
-        max = Math.max(max, sentence.split(" ").length)
-    })
+  //  sentences.forEach(sentence => {
+ //       max = Math.max(max, sentence.split(" ").length)
+ //   })
+    for(let i of sentences){
+        max = Math.max(max, i.split(" ").length)
+    }
+    
     return max
 };
