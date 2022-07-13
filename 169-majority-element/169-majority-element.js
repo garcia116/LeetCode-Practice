124da829374bdf6bf12472e29da8618f67c1ffeb
Gerardo Garcia <38366630+garcia116@users.noreply.github.com>
2022-07-13 16:26:42 -0700

    Time: 84 ms (75.03%), Space: 44.2 MB (43.07%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let seen = {}
-    
-    for (let i = 0; i < nums.length; i++) {
-        seen[nums[i]] = (seen[nums[i]] || 0) + 1
-        if (seen[nums[i]] >= nums.length / 2) return nums[i]
+    let map = {}
+    for(let i=0; i < nums.length; i++){
+        map[nums[i]] = (map[nums[i]] || 0) + 1
+      //  console.log(map[nums[i]])
+        if(map[nums[i]] > nums.length/2) return nums[i]
     }
 };
