0824967d2d6809e496e16de1b58072f93e6d6b10
Gerardo Garcia <38366630+garcia116@users.noreply.github.com>
2022-07-21 11:22:33 -0700

    Time: 1 ms (94.04%), Space: 41.9 MB (64.66%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -9,11 +9,6 @@ class Solution {
         for(String b : word2){
             s2.append(b);
         }
-        System.out.println(s1.toString());
-        System.out.println(s2.toString());
-        s1.toString();
-        s2.toString();
-        
-        return (s1.compareTo(s2) == 0) ? true : false;
+        return (s1.toString().compareTo(s2.toString()) == 0) ? true : false;
     }
 }
